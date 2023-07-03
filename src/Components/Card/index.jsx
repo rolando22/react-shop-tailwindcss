9247fed8278e642b2da07c73286e80c486e60200
rolando22 <rolando2207@gmail.com>
2023-07-03 18:54:37 -0300

    [ADD] fetching de datos de los productos

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,22 +1,22 @@
-export function Card() {
+export function Card({ title, price, image, categoryName }) {
     return (
         <article className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>
-                    Electronics
+                    {categoryName}
                 </span>
                 <img 
                     className='w-full h-full object-cover rounded-lg'
-                    src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    alt='Headphones' 
+                    src={image} 
+                    alt={title} 
                 />
                 <button className='absolute top-0 right-0 grid place-content-center bg-white w-6 h-6 rounded-lg m-2 p-1'>
                     +
                 </button>
             </figure>
             <p className='flex justify-between'>
-                <span className='text-sm font-light'>Headphones</span>
-                <span className='text-lg font-medium'>$3000</span>
+                <span className='text-sm font-light'>{title}</span>
+                <span className='text-lg font-medium'>${price}</span>
             </p>
         </article>
     );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
+
 import { Card } from './../../Components';
 
 export function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch('https://api.escuelajs.co/api/v1/products');
+                const data = await response.json();
+                setProducts(data);
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, []);
+
     return (
         <>
-            <Card />
+            <section className='grid grid-cols-4 gap-4 w-full max-w-screen-lg'>
+                {products.map(product => 
+                    <Card 
+                        key={product.id}
+                        title={product.title}
+                        price={product.price}
+                        image={product.images[0]}
+                        categoryName={product.category.name}
+                    />
+                )}
+            </section>
         </>
     );
 }
